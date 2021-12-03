f727e2f54cbffb880150e061a5e6fc68fc8dc841
aarakelyan19 <90396654+aarakelyan19@users.noreply.github.com>
2021-12-03 12:28:35 +0300

    Update one_hot_encoder_pytest.py

--- HW_tests/issue_4/one_hot_encoder_pytest.py
+++ HW_tests/issue_4/one_hot_encoder_pytest.py
@@ -2,30 +2,25 @@ import pytest
 from one_hot_encoder import fit_transform
 
 
-def test_tf_equal(self):
+def test_tf_equal(*args):
     actual = fit_transform(['Moscow', 'New York', 'Moscow', 'London'])
     expected = [('Moscow', [0, 0, 1]), ('New York', [0, 1, 0]), ('Moscow', [0, 0, 1]), ('London', [1, 0, 0])]
 
     assert expected == actual
-       
-def test_tf_notequal(self):
+          
+def test_tf_notequal(*args):
     actual = fit_transform(['Moscow', 'New York', 'Moscow', 'London'])
     expected = [('Moscow', [0, 0, 1]), ('New York', [0, 1, 0]), ('London', [1, 0, 0])]
 
     assert expected != actual
 
-def test_empty(self):
+def test_empty(*args):
     actual = list(fit_transform(''))
     expected = [('', [1])]
 
     assert expected == actual
 
-def test_NotIn(self):
+def test_NotIn(*args):
     actual = fit_transform(['a', 'b'])
         
-    assert actual not in 'jgj'
-        
-        
-        
-if __name__ == '__main__':
-   pytest.main()
+    assert 'jgj' not in actual
